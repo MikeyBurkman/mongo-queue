996d58ee9167121f301a185a97dd3c73e49bbcbc
MikeyBurkman <michael.burkman@gmail.com>
2016-10-20 10:27:46 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Handles creating a retry queue, and then setting up cron jobs to call it
+
+var retryQueue = require('./lib/queue');
+var retryQueueCron = require('./lib/cron');
+
+module.exports = function(opts) {
+
+  var queue = retryQueue({
+    mongoUrl: opts.mongoUrl,
+    collectionName: opts.collectionName,
+    batchSize: opts.batchSize,
+    maxRecordAge: opts.maxRecordAge,
+    onProcess: opts.onProcess,
+    onFailure: opts.onFailure,
+    retryLimit: opts.retryLimit,
+    conditionFn: conditionFn
+  });
+
+  var processCronJob = retryQueueCron.createJob({
+    name: opts.collectionName + '-process',
+    cron: opts.processCron,
+    fn: queue.processNextBatch
+  });
+
+  var cleanupCronJob = retryQueueCron.createJob({
+    name: opts.collectionName + '-cleanup',
+    cron: opts.cleanupCron,
+    fn: queue.cleanup
+  });
+
+  return {
+    enqueue: queue.enqueue,
+    processNextBatch: processCronJob.run,
+    cleanup: cleanupCronJob.run
+  };
+
+};
