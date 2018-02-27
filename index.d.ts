081f90f564b5e5719bafda0d4e90ed65ae9aa4b2
MikeyBurkman <michael.burkman@gmail.com>
2018-02-27 18:07:02 -0500

    Redo typings

--- index.d.ts
+++ index.d.ts
@@ -3,49 +3,48 @@ declare module 'mongo-queue' {
 
   import { ObjectID } from 'mongodb';
 
-  interface Record<T> {
-    _id: ObjectID;
-    receivedDate: Date;
-    status: 'received'|'processed'|'failed'|'skipped'|'notified'|'notifyFailure';
-    available: Date;
-    data: T;
+  export = MongoQueue;
+
+  function MongoQueue<T>(opts: MongoQueue.InitOpts<T>): MongoQueue.Queue<T>;
+
+  namespace MongoQueue {
+
+    export interface Record<T> {
+      _id: ObjectID;
+      receivedDate: Date;
+      status: 'received'|'processed'|'failed'|'skipped'|'notified'|'notifyFailure';
+      available: Date;
+      data: T;
+    }
+
+    export interface InitOpts<T> {
+      mongoUrl: string;
+      collectionName: string;
+      processCron: string;
+      onProcess: (record: Record<T>) => PromiseLike<any> | undefined;
+      cleanupCron?: string;
+      batchSize?: number;
+      maxRecordAge?: number;
+      retryLimit?: number;
+      backoffMs?: number;
+      backoffCoefficient?: number;
+      onFailure?: (record: Record<T>) => PromiseLike<any> | undefined;
+      continueProcessingOnError?: boolean;
+    }
+
+    export interface Identifiable {
+      _id: ObjectID;
+    }
+
+    export interface Queue<T> {
+      enqueue: (item: T) => PromiseLike<T&Identifiable>;
+      processNextBatch: () => PromiseLike<void>;
+      cleanup: () => PromiseLike<void>;
+      resetRecords: (recordIDs: Array<string>) => PromiseLike<void>;
+    }
+
+    export const skip: (delayMs: number) => any;
+    export const fail: (err: Error) => any;
   }
 
-  interface InitOpts<T> {
-    mongoUrl: string;
-    collectionName: string;
-    processCron: string;
-    onProcess: (record: Record<T>) => PromiseLike<any> | undefined;
-    cleanupCron?: string;
-    batchSize?: number;
-    maxRecordAge?: number;
-    retryLimit?: number;
-    backoffMs?: number;
-    backoffCoefficient?: number;
-    onFailure?: (record: Record<T>) => PromiseLike<any> | undefined;
-    continueProcessingOnError?: boolean;
-  }
-
-  interface Identifiable {
-    _id: ObjectID;
-  }
-
-  interface Queue<T> {
-    enqueue: (item: T) => PromiseLike<T&Identifiable>;
-    processNextBatch: () => PromiseLike<void>;
-    cleanup: () => PromiseLike<void>;
-    resetRecords: (recordIDs: Array<string>) => PromiseLike<void>;
-  }
-
-  interface InitFn {
-    <T>(opts: InitOpts<T>): Queue<T>;
-  }
-
-  interface FlowControl {
-    skip: (delayMs: number) => any;
-    fail: (err: Error) => any;
-  }
-
-  const init: InitFn & FlowControl;
-  export = init;
 }
